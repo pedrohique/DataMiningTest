7a8e0d14ac15e84474872995310d8707ad8c6944
Pedro Moura <pedrohique@hotmail.com>
2023-12-20 12:42:52 -0300

    Update Connect.py

--- DataMining/Connect.py
+++ DataMining/Connect.py
@@ -3,10 +3,7 @@ import tweepy
 
 class ConnectX:
     def __init__(self):
-        self.chave_api = 'Yp3ezjMWJC9OCfZFJjlf9tjU9'
-        self.secret_api = 'WF1rmMoH65uH9KvfcV9QpSrwyv8DR8Wd9aU53Mx3xXfeR0kaOq'
-        self.acess_token = '95677431-RvfVCwXC2YvN7nz1fFmDaDmELoXgxpfJxzjiWkhPW'
-        self.acess_secret = 'dFvl0hQrce9H81LyirPvcnTraxBBNKDws11X1Rg13bA3H'
+        
 
 
     def conecta(self):
